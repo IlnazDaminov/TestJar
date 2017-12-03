d31aa3bdf1307d211e8e2d186587ca6bdd289323
IlnazDamiov <ilnaz.daminov@bk.ru>
2017-12-03 23:48:25 +0300

    TestJar

---
+++ src/Hi.java
@@ -0,0 +1,7 @@
+public class Hi {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.setAge(5);
+        System.out.println("Hello World" +  test.getAge());
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    private int age;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
